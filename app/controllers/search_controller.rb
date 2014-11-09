3aa23caf81bd09f1aac2d10222871851537e7b0e
Kheang Lim <kheang@gmail.com>
2014-11-08 20:35:53 -0500

    Cleaned up merge

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,24 +5,22 @@ class SearchController < ApplicationController
   before_action :get_locations, only: [:index]
 
   def index
-	  @addresses = []
-	  @addresses << "334 Blackwell Street B017, Durham, NC 27701"
-	  @addresses << "201 West Main Street, Durham, NC 27701"
-	  @coordinates = @addresses.map do |address|
-		  coordinate_pair = Geocoder.coordinates(address)
-		  coordinate_hash = {}
-		  coordinate_hash["lat"] = coordinate_pair[0]
-		  coordinate_hash["lng"] = coordinate_pair[1]
-		  coordinate_hash
-	  end
-	  @coordinates = @coordinates
-
-    @locations = HTTParty.get("http://data.raleighnc.gov/resource/c7id-b4bq.json")
+	  address = "334 Blackwell Street B017, Durham, NC 27701"
+		@current_coordinates = Geocoder.coordinates(address)
   end
 
   private
 
   def get_locations
+	  @locations = HTTParty.get("http://data.raleighnc.gov/resource/c7id-b4bq.json")
+	  @coordinates = []
+
+	  @locations.each do |location|
+      latitude = location["location_1"]["latitude"]
+	    longitude = location["location_1"]["longitude"]
+		  @coordinates << [latitude,longitude]
+    end
+
   end
 
 end
